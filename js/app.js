1362312a0949f8120376b41f9ee090751d6deca1
lagomoura <mouralago@live.com>
2023-11-18 22:53:41 -0300

    minors update and bugs fixed

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  // Tu código JavaScript aquí
+
+  // Ejemplo: activar el menú hamburguesa
+  $('.navbar-toggler').click(function() {
+    $('.navbar-collapse').toggleClass('show');
+  });
+});
